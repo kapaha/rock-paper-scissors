f1c8309517444f2f2e0b0105b55b36cfab7d2ce4
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2020-05-27 15:25:32 +0100

    website finished

--- main.js
+++ main.js
@@ -1,35 +1,72 @@
-let playerScore = 0;
+let userScore = 0;
 let computerScore = 0;
 let round = 1;
+let gameFinished = false;
 const maxRounds = 5;
 
-const buttons = document.querySelectorAll('button');
-const currentRound = document.querySelector('#current-round');
-const roundResult = document.querySelector('#round-result');
-const score = document.querySelector('#score');
+const roundParagraph = document.querySelector('.round > p');
+const userScoreSpan = document.getElementById('user-score')
+const computerScoreSpan = document.getElementById('computer-score')
+const resultParagraph = document.querySelector('.result > p');
+const choices = document.querySelectorAll('.choice');
+const actionMessageParagraph = document.getElementById('action-message');
+const resetButton = document.getElementById('reset-btn');
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        game(button.textContent);
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        game(choice.id);
     });
 });
 
-function game(buttonText) {
-    currentRound.textContent = returnCurrentRound();
-    roundResult.textContent = playRound(buttonText, computerChoice());
-    score.textContent = returnScore();
+resetButton.addEventListener('click', (e) => {
+    resetGame()
+});
+
+function game(userChoice) {
+    if(!gameFinished) {
+        resultParagraph.textContent = playRound(userChoice, computerChoice());
+        scoreManager();
+        roundManager();
+    }
 }
 
-function returnCurrentRound() {
-    return `Round: ${round}`;
+function roundManager() {
+    if(round > maxRounds){
+        gameFinished = true;
+        actionMessageParagraph.style = 'font-size: 40px';
+        actionMessageParagraph.textContent = returnFinalScore();
+        resetButton.style.display = 'block';
+    } else {
+        roundParagraph.textContent = `Round ${round}`;
+    }
 }
 
-function returnScore () {
-    return `Player ${playerScore} - ${computerScore} Computer`;
+function scoreManager() {
+    userScoreSpan.textContent = userScore;
+    computerScoreSpan.textContent = computerScore;
+}
+
+function playRound(userSelection, computerSelection) {
+    switch (true) {
+        case userSelection === 'rock' && computerSelection === 'scissors':
+        case userSelection === 'paper' && computerSelection === 'rock':
+        case userSelection === 'scissors' && computerSelection === 'paper':
+            userScore++;
+            round++;
+            return (`You Win! ${userSelection} beats ${computerSelection}.`);
+        case userSelection === 'rock' && computerSelection === 'paper':
+        case userSelection === 'paper' && computerSelection === 'scissors':
+        case userSelection === 'scissors' && computerSelection === 'rock':
+            computerScore++;
+            round++;
+            return (`You Lose! ${computerSelection} beats ${userSelection}.`);
+        case userSelection === computerSelection:
+            return (`You Draw! you both picked ${userSelection}, try again!`);
+    }
 }
 
 function computerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
+    const choice = ['rock', 'paper', 'scissors'];
     return choice[randomNumber(choice.length)];
 }
 
@@ -37,31 +74,25 @@ function randomNumber(maxExclusiveNumber) {
     return Math.floor(Math.random() * maxExclusiveNumber)
 }
 
-function playRound(playerSelection, computerSelection) {
-    switch (true) {
-        case playerSelection === 'Rock' && computerSelection === 'Scissors':
-        case playerSelection === 'Paper' && computerSelection === 'Rock':
-        case playerSelection === 'Scissors' && computerSelection === 'Paper':
-            playerScore++;
-            round++;
-            return (`[Player] ${playerSelection} vs ${computerSelection} [Computer]\nYou Win! ${playerSelection} beats ${computerSelection}.`);
-        case playerSelection === 'Rock' && computerSelection === 'Paper':
-        case playerSelection === 'Paper' && computerSelection === 'Scissors':
-        case playerSelection === 'Scissors' && computerSelection === 'Rock':
-            round++;
-            computerScore++;
-            return (`[Player] ${playerSelection} vs ${computerSelection} [Computer]\nYou Lose! ${computerSelection} beats ${playerSelection}.`);
-        case playerSelection === computerSelection:
-            return (`[Player] ${playerSelection} vs ${computerSelection} [Computer]\nYou Draw! you both picked ${playerSelection}, try again!`);
+function returnFinalScore() {
+    if (userScore > computerScore) {
+        return `You win the game!`;   
+    } else if (userScore < computerScore) {
+        return `You Lose the game!`;
+    } else {
+        return `You Draw the game!`;
     }
 }
 
-// function showFinalScores(playerFinalScore, computerFinalScore) {
-//     if (playerFinalScore > computerFinalScore) {
-//         return `You win the game!\nFinal Score: Player [${playerFinalScore} - ${computerFinalScore}] Computer.`    
-//     } else if (playerScore < computerFinalScore) {
-//         return `You Lose the game!\nFinal Score: Player [${playerFinalScore} - ${computerFinalScore}] Computer.`
-//     } else {
-//         return `You Draw the game!\nFinal Score: Player [${playerFinalScore} - ${computerFinalScore}] Computer.`
-//     }
-// }
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    round = 1;
+    gameFinished = false;
+    actionMessageParagraph.textContent = 'Make your move.';
+    actionMessageParagraph.style = 'font-size: 30px';
+    resultParagraph.textContent = '';
+    scoreManager();
+    roundManager();
+    resetButton.style.display = 'none';
+}
